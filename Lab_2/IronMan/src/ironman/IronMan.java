ac5b101e9db52b018669319c01b8ccb3972adda2
Lia <nathurg3@gmail.com>
2021-12-13 15:55:10 -0500

    Lab 2

---
+++ Lab_2/IronMan/src/ironman/IronMan.java
@@ -0,0 +1,18 @@
+package ironman;
+
+/**
+ *
+ * @author Alejandro Cortázar. Cod:20181020022
+ * @author Andrés Martín. Cod:202010201237
+ * @author Nathalia Rojas. Cod: 20201020124
+ */
+public class IronMan {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+    
+}
